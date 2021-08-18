bded4461f82eae5112bfe67000d4a82f8fda993d
code1218 <skjil1218@gmail.com>
2021-08-18 20:51:34 +0900

    first commit

---
+++ 김준일1916/src/a1_출력/MyInfo.java
@@ -0,0 +1,211 @@
+package a1_출력;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		int studentYear = 2;
+		// sutudentYear (4바이트 정수형)변수에 1이라는 값을 대입한다. 
+		
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+
+		System.out.print("이름: ");
+		System.out.println("김준일");
+		System.out.print("연락처: ");
+		System.out.println("010-9988-1916");
+		System.out.print("주소: ");
+		System.out.println("부산 동래구");
+		System.out.print("학교: ");
+		System.out.println("부산교육대학원");
+		System.out.print("전공: ");
+		System.out.println("컴퓨터교육");
+		System.out.print("학년: ");
+		System.out.println(studentYear);
+		System.out.println();
+		
+		// 연락처: 010-9988-1916
+		// 주소: 부산 동래구
+		// 학교: 부산교육대학원
+		// 전공: 컴퓨터교육
+	}
+
+}
---
+++ 김준일1916/src/a2_변수/MyInfo.java
@@ -0,0 +1,79 @@
+package a2_변수;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		/** <자료형>
+		* 논리형(boolean) = true, false
+		* boolean 변수명 = true;
+		* boolean 변수명 = false;
+		* 
+		* 
+		* 문자형(char) 캐릭터 = a~z, 한글(1글자만)
+		* char 변수명 = 'a';
+		* char 변수명 = '가';
+		* char 변수명 = '1';
+		* char 변수명 = '$';
+		* 
+		* 문자열자료형(String) 스트링 = "문자열을 저장할 수 있습니다."
+		* String 변수명 = "안녕하세요. 김준일입니다.";
+		* 
+		* 정수형(int) Integer = 0 ~ 10 십진수를 표현
+		* int 변수명 = 100;
+		* 
+		* 
+		* 실수형(double) = 0.0 ~ 0.9999 실수단위(소숫점) 표현
+		* double 변수명 = 100.08776;
+		* 
+		*/
+		
+		// 정수형으로 생년월일 중 년도를 저장하는 year변수
+		// 정수형으로 월을 저장하는 month변수
+		// 정수형으로 일을 저장하는 day변수
+		int year = 2021;
+		int month = 8;
+		int day = 11;
+		
+		// 이름을 문자형으로 표현 변수명은 f,s,th
+		char f = '김';
+		char s = '준';
+		char th = '일';
+		String name = "김준일";
+		// 시력 left, right 각각 실수형으로 변수 저장
+		double left = 0.5;
+		double right = 0.7;
+		// 성별 논리형 maleFemale변수명 남성 true, 여성 false;
+		boolean maleFemale = true;
+		
+		// 생년월일: 0000년 00월 00일
+		// 이름: 김준일
+		// 시력(R): 0.5
+		// 시력(L): 0.7
+		// 성별: true
+		
+		System.out.print("생년월일: ");
+		System.out.print(year);
+		System.out.print("년 ");
+		System.out.print(month);
+		System.out.print("월 ");
+		System.out.print(day);
+		System.out.println("일 ");
+		
+		System.out.print("이름: ");
+		System.out.print(f);
+		System.out.print(s);
+		System.out.println(th);
+		
+		System.out.print("이름: ");
+		System.out.println(name);
+		
+		System.out.print("시력(R): ");
+		System.out.println(right);
+		System.out.print("시력(L): ");
+		System.out.println(left);
+
+		System.out.print("성별: ");
+		System.out.println(maleFemale);
+	}
+
+}
