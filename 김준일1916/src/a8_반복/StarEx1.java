1f49fb53b21ba2c5450e4ffd27ce0cfa78c0f6c2
= <=>
2021-08-25 21:56:34 +0900

    2021-08-25 study commit

--- 김준일1916/src/a8_반복/StarEx1.java
+++ 김준일1916/src/a8_반복/StarEx1.java
@@ -5,17 +5,66 @@ import java.util.Scanner;
 public class StarEx1 {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		System.out.print("별의 개수: ");
-		int star = in.nextInt();
-		
-		for(int i = 0; i < star; i++) {
+		/*
+		 * Scanner in = new Scanner(System.in);
+		 * 
+		 * System.out.print("별의 개수: "); int star = in.nextInt();
+		 * 
+		 * for(int i = 0; i < star; i++) { for(int j = 0; j < i + 1; j++) {
+		 * System.out.print("*"); } System.out.println(); }
+		 */
+		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < i + 1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5-i-1; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 5-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < 5; i++) {	
+			for(int j = 0; j < 5 - i + 1; j++)	{				
+				System.out.print(" ");					
+			}
+			for(int j = 0; j < (i*2)+1; j++)	{				
+				System.out.print("*");				
+			}
+			System.out.println();			
+		}	
+		System.out.println();
 	}
 
 }
