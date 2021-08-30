9e746669d457fe970998ff9627c358ae92648b1c
= <=>
2021-08-30 20:52:50 +0900

    20:52 commit

--- 김준일1916/src/b10_클래스/Student.java
+++ 김준일1916/src/b10_클래스/Student.java
@@ -1,20 +1,62 @@
 package b10_클래스;
 
 public class Student {
+
+	private String name;
+	private String schoolName;
+	private int year; //학년
 	
-	public Student() {
-		// TODO Auto-generated constructor stub
+	
+	public Student() { //기본 생성자
+		
+	}
+	
+	public Student(String name,  int year) {
+		super(); //상속 관계
+		
+		this.name = name + " 학생";
+		this.year = year;
+		this.showInfo();
+	}
+	
+	public Student(String name, String schoolName, int year) {
+		super(); //상속 관계
+		
+		this.name = name + " 학생";
+		this.schoolName = schoolName;
+		this.year = year;
+		this.showInfo();
+	}
+	
+	public void setName(String name) {
+		this.name = name + " 학생";
+	}
+	
+	public String getName() {
+		return name;
 	}
 
-	String name;
-	String schoolName;
-	String year; //학년
-	
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
 	public void showInfo() {
 		System.out.println("학생 정보");
 		System.out.println("이름: " + name);
 		System.out.println("학교: " + schoolName);
-		System.out.println("학년: " + "year");
+		System.out.println("학년: " + year);
 	}
 
 }
--- 김준일1916/src/b10_클래스/StudentTest.java
+++ 김준일1916/src/b10_클래스/StudentTest.java
@@ -3,20 +3,17 @@ package b10_클래스;
 public class StudentTest {
 
 	public static void main(String[] args) {
-		Student kim = new Student(); //생성자 -> 클래스명과 같다.
-		
-		kim.name = "김재성";
-		
-		kim.showInfo();
-
+		Student kim = new Student("김준일", 4);
 		Student lee = new Student();
 		
-		lee.name = "이효원";
+		//kim.name = "김준일"; //   .은 해당 메모리 주소값을 참조한다라고 한다.
 		
-		lee.showInfo();
+		kim.showInfo();
 		
-		System.out.println(kim);
-		System.out.println(lee);
+		Integer i = new Integer(100);
+		
+		System.out.println(kim.hashCode());
+		System.out.println(i.hashCode());
 	}
 
 }
